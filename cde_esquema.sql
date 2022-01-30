653e2a4d66ec59893a3302f2bddad063da9e7eef
c3r38r170 <51477979+c3r38r170@users.noreply.github.com>
2022-01-30 14:47:31 +0100

    Add files via upload

---
+++ cde_esquema.sql
@@ -0,0 +1,152 @@
+﻿-- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
+--
+-- Host: localhost    Database: cde
+-- ------------------------------------------------------
+-- Server version	8.0.23
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `acceso`
+--
+
+DROP TABLE IF EXISTS `acceso`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `acceso` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `clienteID` int NOT NULL,
+  `fiestaID` int NOT NULL,
+  `seguridadID` int DEFAULT NULL,
+  `comentarioID` int DEFAULT NULL,
+  `estadoID` int NOT NULL DEFAULT '1',
+  `hora` time DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `clienteID` (`clienteID`),
+  KEY `seguridadID` (`seguridadID`),
+  KEY `estadoID` (`estadoID`),
+  KEY `comentarioID` (`comentarioID`),
+  KEY `acceso_ibfk_5_idx` (`fiestaID`),
+  CONSTRAINT `acceso_ibfk_1` FOREIGN KEY (`clienteID`) REFERENCES `usuarios` (`ID`),
+  CONSTRAINT `acceso_ibfk_2` FOREIGN KEY (`seguridadID`) REFERENCES `usuarios` (`ID`),
+  CONSTRAINT `acceso_ibfk_3` FOREIGN KEY (`estadoID`) REFERENCES `estado` (`ID`),
+  CONSTRAINT `acceso_ibfk_4` FOREIGN KEY (`comentarioID`) REFERENCES `comentario` (`ID`),
+  CONSTRAINT `acceso_ibfk_5` FOREIGN KEY (`fiestaID`) REFERENCES `noche` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `comentario`
+--
+
+DROP TABLE IF EXISTS `comentario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `comentario` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `comentario` varchar(300) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `estado`
+--
+
+DROP TABLE IF EXISTS `estado`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `estado` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL,
+  `color` char(6) CHARACTER SET ascii COLLATE ascii_bin NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `noche`
+--
+
+DROP TABLE IF EXISTS `noche`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `noche` (
+  `ID` int NOT NULL,
+  `fecha` date NOT NULL,
+  `inscripcion` tinyint DEFAULT '1',
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `fecha_UNIQUE` (`fecha`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `rol`
+--
+
+DROP TABLE IF EXISTS `rol`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `rol` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `rol`
+--
+
+LOCK TABLES `rol` WRITE;
+/*!40000 ALTER TABLE `rol` DISABLE KEYS */;
+INSERT INTO `rol` VALUES (1,'Dueño'),(2,'Administrador'),(3,'Seguridad'),(4,'Cliente');
+/*!40000 ALTER TABLE `rol` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuarios` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `usuario` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `contraseña` char(49) CHARACTER SET ascii COLLATE ascii_bin NOT NULL,
+  `nickname` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL,
+  `rolID` int NOT NULL,
+  `creadorID` int DEFAULT NULL,
+  `correo` varchar(75) NOT NULL,
+  `verificado` tinyint(1) NOT NULL DEFAULT '0',
+  `secreto` char(75) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `usuario` (`usuario`),
+  KEY `rolID` (`rolID`),
+  KEY `creadorID` (`creadorID`),
+  CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`rolID`) REFERENCES `rol` (`ID`),
+  CONSTRAINT `usuarios_ibfk_2` FOREIGN KEY (`creadorID`) REFERENCES `usuarios` (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-01-30 10:44:40
