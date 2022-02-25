4f19c33b877d2201650c29a486d4f3329461332f
Santiago Abud <c3r38r170@gmail.com>
2022-02-25 13:02:35 -0300

    Merge branch 'cliente-home'

--- boliche/src/main/java/entidades/Acceso.java
+++ boliche/src/main/java/entidades/Acceso.java
@@ -1,8 +1,11 @@
 package entidades;
 
+import java.sql.Date;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Time;
+import java.util.LinkedList;
 
 import datos.Conexion;
 
@@ -10,22 +13,42 @@ public class Acceso {
 
 	private int ID;
 	private int clienteID;
-	private int nocheID;
+	private Noche noche;
+	public Noche getNoche() {
+		return noche;
+	}
+
+
 	private int seguridadID;
-	private int comentarioID;
+	private String comentario;
+	public String getComentario() {
+		return comentario;
+	}
+
+
 	private int estadoID;
+	public int getEstadoID() {
+		return estadoID;
+	}
+
+
 	private Time hora;
 	
 	public Acceso(int ID) {
 		this.ID=ID;
 		Conexion con=new Conexion();
 		try {
-			ResultSet rs = con.executeSelect("SELECT * FROM acceso WHERE ID="+ID);
+			ResultSet rs = con.executeSelect(
+					"SELECT *"
+					+ " FROM acceso ac"
+					+ " LEFT JOIN comentario com ON ac.comentarioID = com.ID"
+					+ " WHERE ac.ID="+ID);
 			rs.next();
+			//TODO constructor para esto
 			this.clienteID=rs.getInt("clienteID");
-			this.nocheID=rs.getInt("nocheID");
+			this.noche=new Noche(rs.getInt("nocheID"));
 			this.seguridadID=rs.getInt("seguridadID");
-			this.comentarioID=rs.getInt("comentarioID");
+			this.comentario=rs.getString("comentario");
 			this.estadoID=rs.getInt("estadoID");
 			this.hora=rs.getTime("hora");
 		} catch (SQLException e) {
@@ -36,12 +59,11 @@ public class Acceso {
 	
 	public Acceso(int clienteID, int nocheID) {
 		this.clienteID=clienteID;
-		this.nocheID=nocheID;
+		this.noche=new Noche(nocheID);
 		
 		Conexion con=new Conexion();
 		try {
 			con.executeQuery("INSERT INTO acceso (clienteID,nocheID) VALUES ("+clienteID+","+nocheID+")");
-			ID=con.lastInsertID();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -52,4 +74,33 @@ public class Acceso {
 	public int getID() {
 		return ID;
 	}
+	
+	
+	public static LinkedList<Acceso> historialDe(int clienteID){
+
+		LinkedList<Acceso> historial=new LinkedList<Acceso>();
+		ResultSet rs = null;
+		PreparedStatement ps= null;
+		try 
+		{
+			Conexion cn = new Conexion();
+			String query="select ac.ID"
+					+ " from acceso ac"
+					+ " INNER JOIN noche nox ON ac.nocheID = nox.ID"
+					+ " WHERE ac.estadoID!=1"
+					+ " AND ac.clienteID="+clienteID
+					+ " ORDER BY nox.fecha DESC, ac.hora DESC";
+			rs=cn.executeSelect(query);
+			
+		 		while(rs.next())
+		 		{
+		 			Acceso a = new Acceso(rs.getInt(1));
+		 			historial.add(a);
+		 		}
+		}
+		catch(Exception e){System.out.println(e.getMessage());}
+	 
+	      return historial;
+	}
+	
 }
