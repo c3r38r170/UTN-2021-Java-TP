b1f5ee6681cb124c0125a6b2ee84f1d10b0f4f9c
Santiago Abud <c3r38r170@gmail.com>
2022-02-25 22:03:47 -0300

    Limpieza de @SupressWarning("unused")

--- boliche/src/main/java/servlets/Accesos.java
+++ boliche/src/main/java/servlets/Accesos.java
@@ -15,34 +15,21 @@ import entidades.Acceso;
 import entidades.Noche;
 import entidades.Usuario;
 
-/**
- * Servlet implementation class Accesos
- */
 @WebServlet("/accesos")
 @MultipartConfig
 public class Accesos extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+
     public Accesos() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	@SuppressWarnings("unused")
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		int accesoID=Integer.parseInt(request.getParameter("accesoID"));
 		String comentario=request.getParameter("comentario");
